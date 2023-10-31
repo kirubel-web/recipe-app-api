73c89bd300290c071a561aff6f6e8696807a3ea3
kirubel-web <kirubelalemu119@gmail.com>
2023-10-31 17:43:49 +0300

    changes

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularSwaggerView
+    SpectacularSwaggerView,
 )
 from django.contrib import admin
 from django.urls import path
@@ -24,7 +24,8 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path('api/docs/',
-         SpectacularSwaggerView.as_view(url_name='api_schema'),
+    path(
+        'api/docs/',
+         SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
 ]
